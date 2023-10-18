1cf29ef1f4f969815b9602594b75d6f7a8ec6241
LeRoiDesKiwis <ferryantony1@gmail.com>
2023-10-18 18:15:34 +0200

    uwu

--- TP python z3/exercice2.py
+++ TP python z3/exercice2.py
@@ -1,5 +1,7 @@
 from z3 import *
 
+money = 37
+
 p1 = Int("1ct")
 p2 = Int("2cts")
 p3 = Int("5cts")
@@ -9,33 +11,38 @@ p6 = Int("50cts")
 p7 = Int("1eur")
 p8 = Int("2eur")
 
-eq1 = p1*1 + p2*2 + p3*5 + p4*10 + p5*20 + p6*50 + p7*100 + p8*200 == 320
+eq1 = p1*1 + p2*2 + p3*5 + p4*10 + p5*20 + p6*50 + p7*100 + p8*200 == money
 eq2 = And(p1>=0, p2>=0, p3>=0, p4>=0, p5>=0, p6>=0, p7>=0, p8>=0)
 
 solver = Solver()
 solver.add(eq1)
 solver.add(eq2)
 
+solution = (0, 1000000000, [])
 i = 0
-
 while solver.check() == sat:
-    i+=1
-    print("\nSolution", i)
     model = solver.model()
     block = []
-    for var in model:
-        block.append(var() != model[var])
-        solver.add(Or(block))
-    print(p1, " = ", model[p1])
-    print(p2, "= ", model[p2])
-    print(p3, "= ", model[p3])
-    print(p4, "= ", model[p4])
-    print(p5, "= ", model[p5])
-    print(p6,"= ", model[p6])
-    print(p7, "= ", model[p7])
-    print(p8,"= ", model[p8])
+    l = [model[p1].as_long(), model[p2].as_long(), model[p3].as_long(), model[p4].as_long(), model[p5].as_long(), model[p6].as_long(), model[p7].as_long(), model[p8].as_long()]
+    
+    solver.add(Or(p1 != model[p1].as_long(), 
+               p2 != model[p2].as_long(), 
+               p3 != model[p3].as_long(), 
+               p4 != model[p4].as_long(), 
+               p5 != model[p5].as_long(),
+               p6 != model[p6].as_long(),
+               p7 != model[p7].as_long(),
+               p8 != model[p8].as_long())
+               )
 
-    block = []
-    for var in model:
-        block.append(var != model[var])
-        solver.add(block)
+    current_sum = sum(l)
+
+    if current_sum < solution[1]:
+        solution = (i, current_sum, l)
+
+i, number, solut = solution 
+print("\n--- MEILLEURE SOLUTION ---")
+print("Meilleur solution :")
+print(f'- Solution n°{i}')
+print(f'- {number} pièces')
+print(solution)
