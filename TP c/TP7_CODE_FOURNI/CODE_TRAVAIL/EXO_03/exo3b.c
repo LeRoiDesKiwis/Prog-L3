ec97be66e994ecab8b7bee6e601d28fde3ae6bd4
LeRoiDesKiwis <ferryantony1@gmail.com>
2023-11-01 22:24:11 +0100

    TP7 c

--- TP c/TP7_CODE_FOURNI/CODE_TRAVAIL/EXO_03/exo3b.c
+++ TP c/TP7_CODE_FOURNI/CODE_TRAVAIL/EXO_03/exo3b.c
@@ -2,6 +2,10 @@
 #define _XOPEN_SOURCE
 
 #include <stdlib.h>
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
 // mettre les bons includes
 
 int main()
@@ -9,6 +13,10 @@ int main()
     // ici il faut déclarer un sémaphore (en mode IPC_PRIVATE) de deux cases
     // avec les droits 0641
     // et donc on ne le détruit pas
+
+    int semId = semget(IPC_PRIVATE, 2, IPC_CREAT | IPC_EXCL | 0641);
+    // et surtout on n'oublie pas les assert
+    assert(semId != -1);
 
     return EXIT_SUCCESS;
 }
--- TP c/TP7_CODE_FOURNI/CODE_TRAVAIL/EXO_03/exo3d.c
+++ TP c/TP7_CODE_FOURNI/CODE_TRAVAIL/EXO_03/exo3d.c
@@ -2,6 +2,8 @@
 #define _XOPEN_SOURCE
 
 #include <stdlib.h>
+#include <sys/sem.h>
+#include <assert.h>
 // mettre les bons includes
 
 int main()
@@ -9,6 +11,12 @@ int main()
     // ici il faut déclarer un sémaphore (en mode IPC_PRIVATE) de deux cases
     // avec les droits 0641
     // et on le détruit cette fois-ci
+
+    int semId = semget(IPC_PRIVATE, 2, IPC_CREAT | IPC_EXCL | 0641);
+    // et surtout on n'oublie pas les assert
+    assert(semId != -1);
+
+    semctl(semId, -1, IPC_RMID);
 
     return EXIT_SUCCESS;
 }
